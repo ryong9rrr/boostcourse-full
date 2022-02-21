95f60b2c637ef5b5921aa75c385bf0ed0e476577
ryong9rrr <tkddbs1644@gmail.com>
2022-02-21 22:25:34 +0900

    refactor: 템플릿 분리, 함수형 프로그래밍으로 개선

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 // utils
 const $ = (selector) => document.querySelector(selector);
 
+//template
+
+const todoTemplate = (text, idx) => `
+  <li data-todo-id="${idx}" class="mb-3 border-4 border-solid border-stone-500 p-2 bg-yellow-200 shadow-md">
+    <span class="font-semibold">${text}</span>
+    <button class="btn-edit bg-gray-200 px-2 border-2 border-solid border-yellow-500 rounded-md hover:bg-yellow-500">
+      <i class="btn-edit fa-solid fa-pen"></i>
+    </button>
+    <button class="btn-next bg-gray-200 px-2 border-2 border-solid border-green-700 rounded-md hover:bg-green-500">
+      <i class="btn-next fa-solid fa-arrow-right"></i>
+    </button>
+    <button class="btn-remove bg-gray-200 px-2 border-2 border-solid border-red-700 rounded-md hover:bg-red-500">
+      <i class="btn-remove fa-solid fa-xmark"></i>
+    </button>
+  </li>`;
+
+const doingTemplate = (text, idx) => `
+  <li data-doing-id="${idx}" class="mb-3 border-4 border-solid border-stone-500 p-2 bg-green-300 shadow-md">
+    <span class="font-semibold">${text}</span>
+    <button class="btn-edit bg-gray-200 px-2 border-2 border-solid border-yellow-500 rounded-md hover:bg-yellow-500">
+      <i class="btn-edit fa-solid fa-pen"></i>
+    </button>
+    <button class="btn-next bg-gray-200 px-2 border-2 border-solid border-green-700 rounded-md hover:bg-green-500">
+      <i class="btn-next fa-solid fa-arrow-right"></i>
+    </button>
+    <button class="btn-remove bg-gray-200 px-2 border-2 border-solid border-red-700 rounded-md hover:bg-red-500">
+      <i class="btn-remove fa-solid fa-xmark"></i>
+    </button>
+  </li>`;
+
+const doneTemplate = (text, idx) => `
+  <li data-done-id="${idx}" class="mb-3 border-4 border-solid border-stone-500 p-2 bg-pink-200 shadow-md">
+    <span class="font-semibold">${text}</span>
+    <button class="btn-remove bg-gray-200 px-2 border-2 border-solid border-red-700 rounded-md hover:bg-red-500">
+      <i class="btn-remove fa-solid fa-xmark"></i>
+    </button>
+  </li>
+`;
+
 class Store {
   #items;
   constructor() {
@@ -84,6 +123,38 @@ class App {
     this.store = store;
   }
 
+  #listBtnsEventHandler(e) {
+    const $li = e.target.closest("li");
+    if (e.target.classList.contains("btn-edit")) this.#editList($li);
+    if (e.target.classList.contains("btn-next")) this.#moveList($li);
+    if (e.target.classList.contains("btn-remove")) this.#removeList($li);
+  }
+
+  #initEventListener() {
+    $("#to-do-form").addEventListener("submit", (e) => e.preventDefault());
+
+    $("#to-do-form-btn-add").addEventListener(
+      "click",
+      this.#addList.bind(this)
+    );
+    $("#to-do-form-input").addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        return this.#addList();
+      }
+    });
+
+    $("#to-do-list").addEventListener(
+      "click",
+      this.#listBtnsEventHandler.bind(this)
+    );
+  }
+
+  // template: function, data: array
+  #makeHtml(template, data) {
+    const html = data.map(template);
+    return html.join("");
+  }
+
   #getHTMLElementData($li) {
     const parseType = (dataset) => {
       const types = {
@@ -99,49 +170,7 @@ class App {
     return { type, id };
   }
 
-  run() {
-    this.initEventListener();
-    this.render();
-  }
-
-  listBtnsEventHandler(e) {
-    const $li = e.target.closest("li");
-    if (e.target.classList.contains("btn-edit")) this.editList($li);
-    if (e.target.classList.contains("btn-next")) this.moveList($li);
-    if (e.target.classList.contains("btn-remove")) this.removeList($li);
-  }
-
-  initEventListener() {
-    $("#to-do-form").addEventListener("submit", (e) => e.preventDefault());
-
-    $("#to-do-form-btn-add").addEventListener("click", this.addList.bind(this));
-    $("#to-do-form-input").addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        return this.addList();
-      }
-    });
-
-    $("#to-do-list").addEventListener(
-      "click",
-      this.listBtnsEventHandler.bind(this)
-    );
-  }
-
-  renderCount() {
-    $("#todo-count").innerHTML = `${this.store.todosLength} 개`;
-    $("#doing-count").innerHTML = `${this.store.doingsLength} 개`;
-    $("#done-count").innerHTML = `${this.store.donesLength} 개`;
-  }
-
-  render() {
-    this.store.setLocalStorage();
-    this.renderCount();
-    $("#list-todos").innerHTML = this.makeTodosHtml();
-    $("#list-doings").innerHTML = this.makeDoingsHtml();
-    $("#list-dones").innerHTML = this.makeDonesHtml();
-  }
-
-  addList() {
+  #addList() {
     const newTodo = $("#to-do-form-input").value;
     // 사용자 행동 예외처리
     if (newTodo === "") {
@@ -150,115 +179,57 @@ class App {
 
     $("#to-do-form-input").value = "";
     this.store.addItem(newTodo);
-    return this.render();
+    return this.#render();
   }
 
-  removeList($li) {
+  #removeList($li) {
     if (confirm("삭제할까요?")) {
       const { type, id } = this.#getHTMLElementData($li);
       this.store.removeItem(type, id);
-      return this.render();
+      return this.#render();
     }
     return;
   }
 
-  moveList($li) {
+  #moveList($li) {
     if (confirm("완료했나요?")) {
       const text = $li.querySelector("span").textContent;
       const { type, id } = this.#getHTMLElementData($li);
       this.store.moveItem(type, id, text);
-      return this.render();
+      return this.#render();
     }
     return;
   }
 
-  editList($li) {
+  #editList($li) {
     const $span = $li.querySelector("span");
     const prevText = $span.textContent;
     const updatedText = prompt("할 일을 수정할까요?", prevText) || prevText;
     const { type, id } = this.#getHTMLElementData($li);
     this.store.editItem(type, id, updatedText);
-    return this.render();
+    return this.#render();
   }
 
-  makeDonesHtml() {
-    const donesHtmlTemplate = this.store.dones
-      .map((done, index) => {
-        return `
-          <li data-done-id="${index}"
-              class="mb-3 border-4 border-solid border-stone-500 p-2 bg-pink-200 shadow-md"
-          >
-              <span class="font-semibold">${done}</span>
-              <button
-              class="btn-remove bg-gray-200 px-2 border-2 border-solid border-red-700 rounded-md hover:bg-red-500"
-              >
-              <i class="btn-remove fa-solid fa-xmark"></i>
-              </button>
-          </li>
-        `;
-      })
-      .join("");
-    return donesHtmlTemplate;
+  #renderCount() {
+    $("#todo-count").innerHTML = `${this.store.todosLength} 개`;
+    $("#doing-count").innerHTML = `${this.store.doingsLength} 개`;
+    $("#done-count").innerHTML = `${this.store.donesLength} 개`;
   }
 
-  makeDoingsHtml() {
-    const doingsHtmlTemplate = this.store.doings
-      .map((doing, index) => {
-        return `
-          <li data-doing-id="${index}"
-          class="mb-3 border-4 border-solid border-stone-500 p-2 bg-green-300 shadow-md"
-          >
-          <span class="font-semibold">${doing}</span>
-          <button
-              class="btn-edit bg-gray-200 px-2 border-2 border-solid border-yellow-500 rounded-md hover:bg-yellow-500"
-          >
-              <i class="btn-edit fa-solid fa-pen"></i>
-          </button>
-          <button
-              class="btn-next bg-gray-200 px-2 border-2 border-solid border-green-700 rounded-md hover:bg-green-500"
-          >
-              <i class="btn-next fa-solid fa-arrow-right"></i>
-          </button>
-          <button
-              class="btn-remove bg-gray-200 px-2 border-2 border-solid border-red-700 rounded-md hover:bg-red-500"
-              >
-              <i class="btn-remove fa-solid fa-xmark"></i>
-              </button>
-          </li>
-        `;
-      })
-      .join("");
-    return doingsHtmlTemplate;
+  #render() {
+    this.store.setLocalStorage();
+    this.#renderCount();
+    $("#list-todos").innerHTML = this.#makeHtml(todoTemplate, this.store.todos);
+    $("#list-doings").innerHTML = this.#makeHtml(
+      doingTemplate,
+      this.store.doings
+    );
+    $("#list-dones").innerHTML = this.#makeHtml(doneTemplate, this.store.dones);
   }
 
-  makeTodosHtml() {
-    const todosHtmlTemplate = this.store.todos
-      .map((todo, index) => {
-        return `
-      <li data-todo-id="${index}"
-      class="mb-3 border-4 border-solid border-stone-500 p-2 bg-yellow-200 shadow-md"
-      >
-      <span class="font-semibold">${todo}</span>
-      <button
-          class="btn-edit bg-gray-200 px-2 border-2 border-solid border-yellow-500 rounded-md hover:bg-yellow-500"
-      >
-          <i class="btn-edit fa-solid fa-pen"></i>
-      </button>
-      <button
-          class="btn-next bg-gray-200 px-2 border-2 border-solid border-green-700 rounded-md hover:bg-green-500"
-      >
-          <i class="btn-next fa-solid fa-arrow-right"></i>
-      </button>
-      <button
-          class="btn-remove bg-gray-200 px-2 border-2 border-solid border-red-700 rounded-md hover:bg-red-500"
-          >
-          <i class="btn-remove fa-solid fa-xmark"></i>
-          </button>
-      </li>
-  `;
-      })
-      .join("");
-    return todosHtmlTemplate;
+  run() {
+    this.#initEventListener();
+    this.#render();
   }
 }
 
