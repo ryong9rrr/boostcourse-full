5d9b3227d54873eae828ff3d2540d95a105e458d
yong <tkddbs1644@gmail.com>
2022-08-21 21:38:40 +0900

    Hotfix: 엔터로 제출시 생기는 버그 픽스

--- src-localstorage/store.ts
+++ src-localstorage/store.ts
@@ -1,80 +1,81 @@
-import { Items, ItemStore, NextTypes, PrevTypes } from "./types";
+import { Items, ItemStore, NextTypes, PrevTypes } from './types'
 
 export default class Store implements ItemStore {
-  private _items: Items;
+  private _items: Items
 
   constructor() {
     this._items = {
       todos: [],
       doings: [],
       dones: [],
-    };
-    const prevData = this.getLocalStorage();
+    }
+    const prevData = this.getLocalStorage()
     if (prevData) {
       for (const key in prevData) {
-        prevData[key].forEach((v) => this._items[key].push(v));
+        prevData[key].forEach((v) => this._items[key].push(v))
       }
     }
   }
 
   private getNextType(currentType: PrevTypes): NextTypes {
     const types = {
-      ["todos"]: "doings",
-      ["doings"]: "dones",
-    };
-    return types[currentType];
+      ['todos']: 'doings',
+      ['doings']: 'dones',
+    }
+    return types[currentType] as NextTypes
   }
 
   get items() {
-    return this._items;
+    return this._items
   }
 
   get todos() {
-    return this._items.todos;
+    return this._items.todos
   }
 
   get doings() {
-    return this._items.doings;
+    return this._items.doings
   }
 
   get dones() {
-    return this._items.dones;
+    return this._items.dones
   }
 
   get todosLength() {
-    return this._items.todos.length;
+    return this._items.todos.length
   }
 
   get doingsLength() {
-    return this._items.doings.length;
+    return this._items.doings.length
   }
 
   get donesLength() {
-    return this._items.dones.length;
+    return this._items.dones.length
   }
 
   addItem(newItem) {
-    this._items.todos.push(newItem);
+    this._items.todos.push(newItem)
   }
 
   removeItem(type, targetId) {
-    this._items[`${type}`].splice(targetId, 1);
+    this._items[`${type}`].splice(targetId, 1)
   }
 
   moveItem(type, targetId, text) {
-    this._items[`${type}`].splice(targetId, 1);
-    this._items[`${this.getNextType(type)}`].push(text);
+    this._items[`${type}`].splice(targetId, 1)
+    this._items[`${this.getNextType(type)}`].push(text)
   }
 
   editItem(type, targetId, updatedText) {
-    this._items[`${type}`][targetId] = updatedText;
+    this._items[`${type}`][targetId] = updatedText
   }
 
   setLocalStorage() {
-    localStorage.setItem("data", JSON.stringify(this._items));
+    localStorage.setItem('data', JSON.stringify(this._items))
   }
 
   getLocalStorage() {
-    return JSON.parse(localStorage.getItem("data"));
+    const items = localStorage.getItem('data')
+    return items ? JSON.parse(items) : {}
   }
 }
